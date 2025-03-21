d287b495fc54235f6efa54c3915e51c742492457
psalmeight <acelumaad@live.com>
2025-03-22 02:16:33 +0800

    initial commit

---
+++ cgminer.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
+
+type SystemInfo struct {
+	Minertype               string `json:"minertype"`
+	Nettype                 string `json:"nettype"`
+	Netdevice               string `json:"netdevice"`
+	Macaddr                 string `json:"macaddr"`
+	Hostname                string `json:"hostname"`
+	Ipaddress               string `json:"ipaddress"`
+	Netmask                 string `json:"netmask"`
+	Gateway                 string `json:"gateway"`
+	Dnsservers              string `json:"dnsservers"`
+	SystemMode              string `json:"system_mode"`
+	SystemKernelVersion     string `json:"system_kernel_version"`
+	SystemFilesystemVersion string `json:"system_filesystem_version"`
+	FirmwareType            string `json:"firmware_type"`
+	Port                    int    `json:"port"`
+}
+
+func (a *App) PokeMiner(ip string, port int) SystemInfo {
+	var systemInfo SystemInfo
+	url := fmt.Sprintf("http://%s/cgi-bin/get_system_info.cgi", ip)
+	username := "root"
+	password := "root"
+
+	// Create a Resty client (supports Digest Auth)
+	client := resty.New()
+	client.SetTimeout(10 * time.Second)
+
+	// Make the GET request with Digest Authentication
+	resp, err := client.R().
+		SetDigestAuth(username, password). // Digest Auth
+		Get(url)
+
+	if err != nil {
+		fmt.Printf("Error while making request: %v\n", err)
+		return systemInfo
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		fmt.Printf("Unexpected status code: %d, Response: %s\n", resp.StatusCode(), string(resp.Body()))
+		return systemInfo
+	}
+
+	body := resp.Body()
+
+	err = json.Unmarshal(body, &systemInfo)
+	if err != nil {
+		fmt.Printf("Error parsing JSON: %v\n", err)
+		return systemInfo
+	}
+	systemInfo.Port = port
+	runtime.EventsEmit(a.ctx, "responseEvent", systemInfo)
+
+	return systemInfo
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,123 @@
+import { useEffect, useState } from "react";
+import { Start, ToggleManager } from "../wailsjs/go/main/App";
+import { EventsOn, EventsOff } from "../wailsjs/runtime/runtime";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Input,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr
+} from "@chakra-ui/react";
+import { main } from "../wailsjs/go/models";
+function App() {
+  const [ports, _setPorts] = useState<number[]>([1234, 3434, 8888, 4555]);
+  const [listeningPorts, setListeningPorts] = useState<
+    { port: number; isListening: boolean; received: main.SystemInfo[] }[]
+  >([
+    { port: 1234, isListening: false, received: [] },
+    { port: 3434, isListening: false, received: [] },
+    { port: 8888, isListening: false, received: [] },
+    { port: 4555, isListening: false, received: [] }
+  ]);
+
+  const togglePortListen = (port: number) => {
+    if (listeningPorts.every(({ isListening }) => !isListening)) {
+      Start(port);
+    }
+
+    ToggleManager(port);
+    console.log("isListening ", listeningPorts.find((p) => p.port === port)?.isListening);
+    if (listeningPorts.find((p) => p.port === port)?.isListening) {
+      setListeningPorts((prev) => prev.map((p) => (p.port === port ? { ...p, isListening: false } : p)));
+    } else {
+      setListeningPorts((prev) => prev.map((p) => (p.port === port ? { ...p, isListening: true } : p)));
+    }
+  };
+
+  useEffect(() => {
+    EventsOn("responseEvent", (data: main.SystemInfo) => {
+      setListeningPorts((prev) =>
+        prev.map((response) => {
+          if (response.port === data.port) {
+            return { ...response, received: [...response.received, data] };
+          }
+          return response;
+        })
+      );
+    });
+
+    // Cleanup listener when the component unmounts
+    return () => {
+      EventsOff("responseEvent");
+    };
+  }, []);
+
+  return (
+    <Flex p={4} gap={4} height="100vh">
+      <Box w="30%" p={4} borderWidth="1px" borderRadius="lg" boxShadow="md">
+        {/* <Input placeholder="Enter port" mb={4} size="md" onChange={(e) => setPort(Number(e.target.value))} />
+        <Button colorScheme="blue" width="full" onClick={() => {
+          if (port) {
+            startListening
+        }}>
+          {port ? "Stop Listening" : "Start Listening"}
+        </Button> */}
+      </Box>
+      <Box w="70%" p={4} borderWidth="1px" borderRadius="lg" boxShadow="md" height="fit-content">
+        {listeningPorts.map((lPort) => (
+          <Card key={lPort.port} mb={4}>
+            <CardHeader pb={0}>
+              <Text fontSize="lg" fontWeight="bold" margin={0}>
+                Port: {lPort.port} {lPort.isListening ? "(Listening)" : ""}
+              </Text>
+              <Button size="sm" onClick={() => togglePortListen(lPort.port)}>
+                {lPort.isListening ? "Stop" : "Listen"}
+              </Button>
+            </CardHeader>
+            <CardBody>
+              <TableContainer>
+                <Table variant="simple">
+                  <Thead>
+                    <Tr>
+                      <Th>MinerType</Th>
+                      <Th>IPAddress</Th>
+                      <Th>Mac</Th>
+                      <Th>Port</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {lPort.received.length > 0
+                      ? lPort.received.map((r: main.SystemInfo) => {
+                          return (
+                            <Tr>
+                              <Td>{r.minertype}</Td>
+                              <Td>{r.ipaddress}</Td>
+                              <Td>{r.macaddr}</Td>
+                              <Td>{r.port}</Td>
+                            </Tr>
+                          );
+                        })
+                      : null}
+                    {/* Add more rows as needed */}
+                  </Tbody>
+                </Table>
+              </TableContainer>
+            </CardBody>
+          </Card>
+        ))}
+      </Box>
+    </Flex>
+  );
+}
+
+export default App;
